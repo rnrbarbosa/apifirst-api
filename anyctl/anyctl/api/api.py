1f02a97cbe3af39c308b78b78ca5eea2535579f3
Roberto Barbosa <rnrbarbosa@gmail.com>
2020-04-26 13:24:24 +0200

    changed flask for aiohttp

--- anyctl/anyctl/api/api.py
+++ anyctl/anyctl/api/api.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import connexion
+from aiohttp import web
 
 if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__)
-    app.add_api('openapi.yaml')
-    app.run(port=8080)
+    app = connexion.AioHttpApp(__name__, port=8080, specification_dir='./')
+    app.add_api('openapi.yaml', arguments={'title': 'Hello World Example'})
+    app.run()
